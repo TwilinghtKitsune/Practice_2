1a3f0a1b2ed085b3408b99843fcf2abc0c91316a
Ekaterina <Eamaltseva14@gmail.com>
2022-03-26 19:30:09 +0400

    First commit

---
+++ Practice_2/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Practice_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practice_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(TextBoxes form)
+        {
+            if (ModelState.IsValid)
+            {
+                if (form.Password_1 == form.Password_2)
+                {
+                    if(Convert.ToInt32(form.Age) >= 18 && Convert.ToInt32(form.Age) <= 65)
+                    {
+                        Session["login"] = form.Login;
+                        Session["password"] = form.Password_1;
+                        return View("Session");
+                    }
+                    else ViewBag.Wrong = "Вы ввели некорректный возраст";
+                }
+                else ViewBag.Wrong = "Пароли не совпадают";
+            }
+            return View();
+        }
+    }
+}
---
+++ Practice_2/Models/TextBoxes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Practice_2.Models
+{
+    public class TextBoxes
+    {
+        [Required(ErrorMessage = "Пожалуйста, введите логин")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите пароль")]
+        public string Password_1 { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите пароль")]
+        public string Password_2 { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите email")]
+        [RegularExpression(@"^(?("")(""[^""]+?""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9]{2,17}))$", ErrorMessage = "Вы ввели некорректный email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите возраст")]
+        [RegularExpression("[0-9]{2}", ErrorMessage = "Вы ввели некорректный возраст")]
+        public string Age { get; set; }
+    }
+}
